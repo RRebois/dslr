56fd8c3184f3e1aa531b46bd9ce75e3b223cddd6
rrebois <rolando.rebois@yahoo.fr>
2025-01-31 09:03:22 +0100

    csv error corrected

--- logreg_predict.py
+++ logreg_predict.py
@@ -1,6 +1,8 @@
 import sys
 import pandas as pd
 from pandas import DataFrame
+import pickle
+import numpy as np
 
 
 def load(path: str) -> DataFrame | None:
@@ -21,6 +23,69 @@ def load(path: str) -> DataFrame | None:
         raise FileNotFoundError(path)
 
 
+def load_thetas(s: str) -> dict:
+    """
+    Loads thetas for the predictions.
+    :param s: path to thetas file.
+    :return: dictionary of thetas.
+    """
+    try:
+        with open(sys.argv[2], "rb") as f:
+            weights = pickle.load(f)
+    except FileNotFoundError:
+        raise FileNotFoundError(f"No such file or directory: {s}")
+    return weights
+
+
+def standardization(df: DataFrame, titles: list) -> np.ndarray:
+    """
+    Normalize the data using standardization normalization.
+    :param df: DataFrame
+    :param titles: list of features
+    :return: NumPy array
+    """
+    X = df[titles].to_numpy()
+    Xmean = X.mean(axis=0)
+    Xstd = X.std(axis=0)
+    X = (X - Xmean) / Xstd
+    return X
+
+
+def sigmoid(x: np.ndarray) -> np.ndarray:
+    """
+    Computes the sigmoid function
+    :param x: linear regression output
+    :return: NumPy array of the sigmoid function values
+    """
+    return 1 / (1 + np.exp(-x))
+
+
+def ft_predict(df: DataFrame, thetas: dict, titles: list) -> None:
+    """
+    Predicts the houses of students.
+    :param df: dataframe
+    :param thetas: dictionary of thetas for each feature
+    :param titles: list of features
+    :return:
+    """
+    houses = list(thetas.keys())
+
+    # Standardize data to match thetas
+    X = standardization(df, titles)
+
+    # Save results in a dict for each house
+    results = {house: sigmoid(np.dot(X, thetas[house][1:]) +
+                            thetas[house][0]) for house in houses}
+
+    # fill df['Hogwarts House'] with house having highest result
+    df['Hogwarts House'] = [max(results, key=lambda house:
+    results[house][i]) for i in range(len(df))]
+
+    print(df['Hogwarts House'])
+    df.insert(0, "Index", df.index) # if clean data remove this line else add it
+    df.to_csv("houses_st.csv", columns=["Index", "Hogwarts House"], index=False)
+
+
 def clean_data(df: DataFrame, titles: list) -> None:
     """
     Clean the data by replacing nan values by mean.
@@ -32,14 +97,10 @@ def clean_data(df: DataFrame, titles: list) -> None:
     df[titles] = df[titles].fillna(df[titles].mean())
 
 
-def load_thetas(s: str) -> dict:
-
-
-
 def main():
     try:
         assert len(sys.argv) == 3, ("Wrong number of arguments"
-                "python3 logreg_train.py <path-to-data-test> <path-to-thetas>")
+                "python3 logreg_train.py test_file thetas_file")
         df = load(sys.argv[1])
         titles = [
             #'Astronomy',
@@ -52,10 +113,13 @@ def main():
             'Charms',
             #'Flying'
         ]
-        #df.dropna(inplace=True)
-        clean_data(df, titles) # filling the nan values with mean or drop them??
+        df = df.iloc[:, 6:]
+        df.dropna(inplace=True)
+        print(df.head())
+        #clean_data(df, titles) # filling the nan values with mean or drop them??
         thetas = load_thetas(sys.argv[2])
-        train_logreg(df, titles)
+        ft_predict(df, thetas, titles)
+        print(df)
 
     except Exception as e:
         print("Error:", e)
