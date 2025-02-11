3f39d97a0ebc8fc58b2d0f2e9b0035c78a352318
Tanguy Gellon <tanguy.gellon@gmail.com>
2025-02-11 17:48:15 +0100

    [WIP] Implementing other GD algorithms

--- README.md
+++ README.md
@@ -28,15 +28,45 @@ We have a dataset of 1600 previous students, with 14 features to train our model
 - Charms
 - Flying
 
+After analyzing the data, we can see that some features allow to classify the students 
+in a house more easily than others. For example, the feature `History of Magic` 
+has a high correlation with the house Gryffindor, or `Muggle Studies` with Ravenclaw.
+
+This allows us to select the most relevant features (we kept 9) to train our model and increase its accuracy.
+
 ## Logistic Regression
 
-The logistic regression algorithm is a classification algorithm: it can predict the probability of an event to be true or false. In our case, it is a multi-classification, e.g. will the student be in Gryffindor, Slytherin, Ravenclaw or Hufflepuff.
-We use the One vs All method to train the model: For each house, we divide the samples in 2 groups: `1` if the student is in the house, `0` otherwise.
+The logistic regression algorithm is a classification algorithm: it can predict 
+the probability of an event to be true or false. In our case, it is a multi-classification, 
+e.g. will the student be in Gryffindor, Slytherin, Ravenclaw or Hufflepuff. 
+We use the One vs All method to train the model: For each house, we divide the samples 
+in 2 groups: `1` if the student is in the house, `0` otherwise. 
 
-The logistic regression model is based on the sigmoid function, which is a function that maps any real value into a value between 0 and 1.
+The logistic regression model is based on the sigmoid function, which is a function 
+that maps any real value into a value between 0 and 1. 
+
+As in the linear regression, we use the gradient descent algorithm to minimize the 
+cost function and increase the accuracy of the model. 
+
+## Usage
+
+For these programs to work, you need to have the following libraries installed:
+`pandas`, `numpy`, `mathplotlib`, `seaborn`, `tqdm`
+
+- To run `describe.py`, `heatmap.py`, `pairplot.py` and `scatterplot.py` :
+
+        python3 describe.py data/dataset_train.csv
+
+- To run `logreg_train.py` :
+
+        python3 logreg_train.py data/dataset_train.csv
 
 ## Ressources
 
 https://datascientest.com/regression-logistique-quest-ce-que-cest
 
 https://www.ibm.com/fr-fr/topics/logistic-regression
+
+https://mrmint.fr/logistic-regression-machine-learning-introduction-simple
+
+https://www.geeksforgeeks.org/ml-stochastic-gradient-descent-sgd/
--- logreg_train.py
+++ logreg_train.py
@@ -65,15 +65,86 @@ def model_accuracy(X: np.ndarray, thetas: dict, col: pd.Series) -> float:
     print("Model accuracy: ", "{:.3f}".format(acc))
 
 
-def train_logreg(df: DataFrame, titles: list) -> None:
+def batch_gd(theta: np.ndarray, intercept: float, X: np.ndarray, y: np.ndarray, cost: list) -> tuple:
+    """
+    Batch gradient descent algorithm
+    :param theta:
+    :param intercept:
+    :param X:
+    :param y:
+    :param cost:
+    :return:
+    """
+
+    lr = 0.025
+    n_iters = 500
+    for iter in range(n_iters):
+        y_pred = sigmoid(np.dot(X, theta) + intercept)
+
+        dw = (1 / len(X)) * np.dot(X.T, (y_pred - y))
+        db = (1 / len(X)) * np.sum(y_pred - y)
+
+        theta -= lr * dw
+        intercept -= lr * db
+
+        loss = (-1 / len(X)) * np.sum(
+            y * np.log(y_pred) + (1 - y) * np.log(1 - y_pred))
+        cost.append(loss)
+
+        if iter % 100 == 0:
+            # current_accuracy = accuracy(sigmoid(np.dot(X, theta) +
+            #                                    intercept) >= 0.7, y)
+            print("iter:", iter, "cost:", "{:.2f}".format(loss))
+
+    return intercept, cost
+
+
+def stochastic_gd(theta: np.ndarray, intercept: float, X: np.ndarray, y: np.ndarray, cost: list) -> tuple:
+    """
+    Stochastic gradient descent algorithm
+    :param theta:
+    :param intercept:
+    :param X:
+    :param y:
+    :param cost:
+    :return:
+    """
+
+    lr = 0.025
+    n_iters = 500
+    for iter in range(n_iters):
+        total_loss = 0
+        for i in range(len(X)):
+            index = np.random.randint(0, len(X))
+            x_i = X[index]
+            y_i = y[index]
+
+            y_pred = sigmoid(np.dot(x_i, theta) + intercept)
+
+            dw = x_i * (y_pred - y_i)
+            db = y_pred - y_i
+
+            theta -= lr * dw
+            intercept -= lr * db
+
+            loss = y_i * np.log(y_pred) + (1 - y_i) * np.log(1 - y_pred)
+            total_loss += loss
+            cost.append(total_loss / len(X))
+
+            if iter % 100 == 0:
+                print("iter:", iter, "cost:", "{:.2f}".format(loss))
+
+    return intercept, cost
+
+
+def train_logreg(df: DataFrame, titles: list, algo: str) -> None:
     """
     Train a logreg model
     :param df: DataFrame
     :param titles: list of feature names
+    :param algo: str
     :return: None
     """
-    lr = 0.025
-    n_iters = 500
     thetas = {}
     costs = {}
 
@@ -83,7 +154,7 @@ def train_logreg(df: DataFrame, titles: list) -> None:
 
     houses = df['Hogwarts House'].unique()
 
-    # Perform oneVSall model for each class
+    # Perform one VS all model for each class
     for house in tqdm(houses):
         print("Training logreg for class", house)
         time.sleep(1)
@@ -101,22 +172,10 @@ def train_logreg(df: DataFrame, titles: list) -> None:
         cost = []
 
         # Gradient descent
-        for iter in range(n_iters):
-            y_pred = sigmoid(np.dot(X, theta) + intercept)
-
-            dw = (1 / len(X)) * np.dot(X.T, (y_pred - y))
-            db = (1 / len(X)) * np.sum(y_pred - y)
-
-            theta -= lr * dw
-            intercept -= lr * db
-
-            loss = (-1 / len(X)) * np.sum(y * np.log(y_pred) + (1 - y) * np.log(1 - y_pred))
-            cost.append(loss)
-
-            if iter % 100 == 0:
-                #current_accuracy = accuracy(sigmoid(np.dot(X, theta) +
-                #                                    intercept) >= 0.7, y)
-                print("iter:", iter, "cost:", "{:.2f}".format(loss))
+        if algo == "batch":
+            intercept, costs = batch_gd(theta, intercept, X, y, cost)
+        elif algo == "stochastic":
+            intercept, costs = stochastic_gd(theta, intercept, X, y, cost)
 
         thetas[house] = theta
         # Add intercept to theta values
@@ -155,9 +214,13 @@ def clean_data(df: DataFrame, titles: list) -> None:
 
 
 def main():
-    try:
-        assert len(sys.argv) == 2, ("Wrong number of arguments, usage : "
-                                    "python3 logreg_train.py <path-to-data>")
+    # try:
+        assert len(sys.argv) == 3, ("Wrong number of arguments, usage : "
+                                    "python3 logreg_train.py <path-to-data> <algorithm>")
+        assert (sys.argv[2] == "batch" or sys.argv[2] == "stochastic"
+                or sys.argv[2] == "mini-batch"), \
+                "Wrong algorithm entered, must be 'batch', 'stochastic' or 'mini-batch'"
+        algo = sys.argv[2]
         df = load(sys.argv[1])
         titles = [
             'Astronomy',
@@ -172,10 +235,10 @@ def main():
         ]
         #df.dropna(inplace=True)
         clean_data(df, titles) # filling the nan values with mean or drop them??
-        train_logreg(df, titles)
+        train_logreg(df, titles, algo)
 
-    except Exception as e:
-        print("Error:", e)
+    # except Exception as e:
+    #     print("Error:", e)
 
 
 if __name__ == "__main__":
